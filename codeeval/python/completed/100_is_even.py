d4dcce79b26285bf25e373050cbbdac6591fbd13
T.A. Ebesu <tebesu@gmail.com>
2015-02-11 13:00:37 -0800

    renamed iseven, improved speed

--- codeeval/python/completed/100_is_even.py
+++ codeeval/python/completed/100_is_even.py
@@ -3,14 +3,10 @@
 @author:   Travis A. Ebesu
 @created:  2014-07-27
 @summary:  Codeeval, checks if a number is even. 1 for even, 0 false
+https://www.codeeval.com/open_challenges/100/
 '''
-
-
-
 import sys
 f = open(sys.argv[1], 'r')
+# 18 ms improvement
 for i in f:
-    if int(i) % 2 == 0:
-        print '1'
-    else:
-        print '0'
+    print 1 if int(i) % 2 == 0 else 0
