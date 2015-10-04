27a228ff649d39b727212a307f3ce2dbce7ef774
T.A. Ebesu <tebesu@gmail.com>
2015-10-04 12:15:06 -0700

    fixed sum_to with a hash table instead, bug existed

--- misc/sum_to.py
+++ misc/sum_to.py
@@ -11,17 +11,30 @@ def sum_to(n_list, s):
     Given a list of n numbers, find if any two numbers sum to s in O(n)
     Total time is: O(2n) ~ O(n)
     '''
-    array = [0] * s # O(n)
+    table = dict()
 
     for num in n_list: # O(n)
-        rem = s-num
-        if array[num] or array[rem]:
-            return True
-        array[rem] = 1
+        if num < s:
+            table[num] = s - num
+
+    for key in table.keys(): # O(n)
+        # Checking key should be O(1) depends on implementation
+        if table[key] in table: # Check if remainder in table?
+            return key, table[key]
     return False
 
-assert not sum_to([1, 3, 2, 5], 10)
-assert sum_to([1, 3, 2, 5], 7)
+import numpy as np
+# np.random.seed(0)
+high = 50
+n = 10
+a = np.random.randint(0, n, 2).ravel()
+x = np.random.randint(0, high, n).ravel()
+
+ans = x[a[0]] + x[a[1]]
+print
+print x
+print ans
+print sum_to(x, ans)
 
 
 
